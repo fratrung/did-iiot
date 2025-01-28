3e97a4ea1aa3cdf66355fb0f7f9b8f0b920f7e70
ciccio.trung <francesco.trung@gmail.com>
2025-01-28 01:50:00 +0100

    typo corrected

--- modules.py
+++ modules.py
@@ -8,4 +8,4 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from did_iiot.did_iiot.did_iiot import DIDIndustrialIoT
 from did_iiot.did_iiot.did_document import DIDDocument, Service, VerificationMethod
-from did_iiot.did_iiot.publicjwk import PublicJwKey, KyberPublicJwkey, DilithiumPublicJwkey, RSAPublicJwkey, Ed25519PubliJwkey
+from did_iiot.did_iiot.publicjwk import PublicJwKey, KyberPublicJwkey, DilithiumPublicJwkey, RSAPublicJwkey, Ed25519PublicJwkey
